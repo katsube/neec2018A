c7c059cdb9f55dd5448188e7d18ee6c892bff68f
Makito Katsube <katsubemakito@gmail.com>
2018-06-13 01:27:31 +0900

    Update add.php

--- session/add.php
+++ session/add.php
@@ -1,12 +1,15 @@
 ﻿<?php
-$sid = $_COOKIE['sessionid'];		//CookieからセッションIDを取得
-$pid = $_GET['pid'];				//商品ID
+//-------------------------------------
+// 値を取得
+//-------------------------------------
+$sid = $_COOKIE['sessionid'];   //CookieからセッションIDを取得
+$pid = $_GET['pid'];            //商品ID
 
 //-------------------------------------
 // ファイルに記録
 //-------------------------------------
-$file = sprintf('data/%s.txt', $sid);
-$fp = fopen($file, 'a');
+$file = sprintf('data/%s.txt', $sid);   //本来はブラウザから見える場所にデータファイルを置いちゃダメです
+$fp   = fopen($file, 'a');
 flock($fp, LOCK_EX);
 fwrite($fp, implode(',', [$pid, time()]));
 fwrite($fp, "\n");
